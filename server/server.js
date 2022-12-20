372b0780b98fc65fb89fb7028646ce22b799708f
Ryan Cokely <ryan.cokely@gmail.com>
2022-12-20 16:15:16 -0600

    routes setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const express = require("express")
+const db = require("./db/index")
 const morgan = require("morgan")
 const app = express()
 
@@ -8,52 +9,82 @@ app.use(morgan("dev"))
 app.use(express.json())
 
 //get all restaurants
-app.get("/home", (req, res) => {
-  res.status(200).json({
-    status: "sucess",
-    data: {
-      restaurant: ["mcdonalds", "wendys"]
-    }
-  }
+app.get("/home", async (req, res) => {
+  try{
+    const results = await db.query('select * from places')
+    res.status(200).json({
+      status: "sucess",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows
+      },
+    });
+  } catch (err){
+    console.log(err)
+  }}
+
+
   )
-})
 
-app.get("/home/:id", (req, res) => {
- console.log(req.params)
+app.get("/home/:id", async (req, res) => {
+ try{
+ const results = await db.query("select * from places where id= $1", [req.params.id])
  res.status(200).json({
-  status: "success",
+  status: "sucess",
   data: {
-    restaurant: "Sams"
+    restaurants: results.rows[0]
+  },
+});
+} catch (err){
+console.log(err)
+}}
+
+
+)
+
+app.post("/home", async (req, res) => {
+  try{
+    const results = await db.query("INSERT INTO PLACES (name, location, pricerange) values ($1, $2, $3) returning *", [req.body.name, req.body.location, req.body.pricerange])
+    console.log("RES", results)
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+     })
+
+  } catch (err){
+    console.log(err)
   }
- })
 })
 
-app.post("/home", (req, res) => {
-  console.log(req.body)
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: "Sams"
-    }
-   })
-})
-
-app.delete("/home/:id", (req, res) => {
-  res.status(204).json({
-    status: "deleted"
-  })
- })
+app.delete("/home/:id", async (req, res) => {
+  try{
+    const results = await db.query("DELETE from places where id= $1", [req.params.id])
+    res.status(204).json({
+      status: "deleted"})
+   } catch (err){
+   console.log(err)
+   }}
+  )
 
 
- app.put("/home/:id", (req, res) => {
-  console.log(req.params.id);
-  console.log(req.body)
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "Sams"
-    }
-   })
+
+ app.put("/home/:id", async(req, res) => {
+  try{
+    const results = await db.query("UPDATE places SET name = $1, location = $2, pricerange = $3 where id = $4 returning *;", [req.body.name, req.body.location, req.body.pricerange, req.params.id])
+    console.log(results)
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0]
+      }
+     })
+
+  } catch (err){
+    console.log(err)
+  }
+
  })
 
 
